cbcd829f9f92d0dfc92149ca29930670d3dda364
Dardan Gjonbalaj <xdundun@gmail.com>
2019-10-01 17:47:00 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = [ snickers, hundred grand, kitkat, skittles];
+  
+]
+
+function addElementToBeginningOfArray() {
+  
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+  
+}
