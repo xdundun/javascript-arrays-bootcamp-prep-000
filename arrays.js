e98ae80507c8f92ccab2980884f01f9d04f1544a
Dardan Gjonbalaj <xdundun@gmail.com>
2019-10-01 17:59:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,6 +2,7 @@ var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(foo, element) {
   foo.push(element);
+  return foo
 }
 
 function destructivelyAddElementToBeginningOfArray() {
