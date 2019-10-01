6fbe696348453d9bade0fee59fb7000efaeaa0e1
Dardan Gjonbalaj <xdundun@gmail.com>
2019-10-01 17:49:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,3 +7,23 @@ function addElementToBeginningOfArray() {
 function destructivelyAddElementToBeginningOfArray() {
 
 }
+
+function addElementToEndOfArray() {
+  
+}
+
+function destructivelyAddElementToEndOfArray() {
+  
+}
+
+function accessElementInArray(){
+  
+}
+
+function destructivelyRemoveElementFromBeginningOfArray() {
+  
+}
+
+function removeElementFromBeginningOfArray() {
+  
+}
